ccc54b46f9855f5bbee8968d98770b18cad60000
krypt0bash <cosminsebastian2004@gmail.com>
2023-06-03 18:00:17 +0300

    changes

--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 export const announceDiscordWebhook = async (title: string) => {
-	await axios.post(
-		'https://discord.com/api/webhooks/1109516958560424016/1X_hpvWZEh5w416xcw96T-xPLCCsrh5z4hyLiT6R-VN9rYM6Qw3I34JcPMhdhBQyxwZB',
-		{
-			embeds: [{ title, color: '3080403' }],
-		}
-	);
+	await axios.post(process.env.DISCORD_WEBHOOK as string, {
+		embeds: [{ title, color: '3080403' }],
+	});
 };
