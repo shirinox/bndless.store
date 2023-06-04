d9484077c629a562a22860ca1b078c8d480fc8b2
krypt0bash <cosminsebastian2004@gmail.com>
2023-06-04 16:55:01 +0300

    Added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
 	title: 'bndless.store | Boundless Market | cheapest league of legends accounts',
+	description: '',
 	icons: {
 		icon: '/icon.png',
 	},
@@ -16,6 +17,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang='en'>
+			<meta
+				name='description'
+				content='Welcome to our Smurf League of Legends Store! Find a wide selection of level 30 unranked accounts, rare skins, and more. Boost your gameplay and dominate the Rift with our high-quality smurf accounts.'
+			/>
 			<body className={inter.className}>
 				<div className='max-w-sm md:max-w-2xl xl:max-w-4xl 2xl:max-w-7xl mx-auto'>
 					<Navigation />
